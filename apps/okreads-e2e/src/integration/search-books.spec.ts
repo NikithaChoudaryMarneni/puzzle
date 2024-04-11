1d2fc8df057db6a35444e21601f3415aff9eac6a
NikithaChoudaryMarneni <nikhita.choudarymarneni1@t-mobile.com>
2024-04-11 02:27:18 -0400

    Task 3: Undo add and remove from reading list

--- apps/okreads-e2e/src/integration/search-books.spec.ts
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -1,33 +1,33 @@
-import { $, $$, browser, ExpectedConditions } from 'protractor';
-
 describe('When: Use the search feature', () => {
-  it('Then: I should be able to search books by title', async () => {
-    await browser.get('/');
-    await browser.wait(
-      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
-    );
-    const form = await $('form');
-    const input = await $('input[type="search"]');
-    await input.sendKeys('javascript');
-    await form.submit();
-    const items = await $$('[data-testing="book-item"]');
-    expect(items.length).toBeGreaterThan(1);
+  beforeEach(() => {
+    cy.startAt('/');
   });
 
-  it('Then: I should see search results as I am typing', async () => {
-    await browser.get('/');
-    await browser.wait(
-      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
-    );
+  it('Then: I should be able to search books by title', () => {
+    // Type the search term
+    cy.get('input[type="search"]').type('javascript');
+  
+    // Submit the form
+    cy.get('form').submit();
+  
+    // Wait for the search results to appear
+    cy.wait(3000);
+  
+    // Check if there are more than one book items displayed
+    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
 
-    const form = await $('form');
-    const input = await $('input[type="search"]');
+  }); 
 
-    // type in search text without form submission results should appear
-    await input.sendKeys('java');
+  it('Then: I should see search results as I am typing', () => {
+    const searchTerm = 'javascript';
 
-    const items = await $$('[data-testing="book-item"]');
-    expect(items.length).toBeGreaterThan(1);
+    cy.get('input[type="search"]').type(searchTerm);
+    cy.get('form').submit();
+
+    // Wait for search results to be fetched and rendered
+    cy.wait(3000); // Adjust the wait time as needed
+
+    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 0);
   });
 
 });
