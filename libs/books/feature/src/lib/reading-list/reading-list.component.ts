87967b7fb645d9fd3f9c79f92e6ff9794f22e839
NikithaChoudaryMarneni <nikhita.choudarymarneni1@t-mobile.com>
2024-04-10 23:41:19 -0400

    changes for Task 1: Code fixes and review

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { ReadingListItem } from '@tmo/shared/models';
 
 @Component({
   selector: 'tmo-reading-list',
@@ -9,6 +10,7 @@ import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
 })
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
+  trackReadingListItem = (index: number, list: ReadingListItem) => index;
 
   constructor(private readonly store: Store) {}
 
